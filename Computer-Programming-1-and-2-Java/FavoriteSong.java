5bc68aab70171ed4ac6862ed5f8da29735dd05a9
Erika Lastierre <aessu.0126@gmail.com>
2026-01-22 15:17:19 +0800

    Add files via upload

---
+++ Computer-Programming-1-and-2-Java/FavoriteSong.java
@@ -0,0 +1,9 @@
+//basic syntax in java, using println to display multiple lines of text
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("That if you say goodbye today,");
+        System.out.println("I'd ask you to be true");
+        System.out.println("Cause the hardest part of this");
+        System.out.println("is leaving you");
+    }
+}
